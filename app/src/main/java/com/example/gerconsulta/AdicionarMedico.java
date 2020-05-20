ff8957f3c9e45f1f3cb89c395cd82c8c9170515c
wagnerlopes-univ <63763286+wagnerlopes-univ@users.noreply.github.com>
2020-05-19 23:21:53 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/gerconsulta/AdicionarMedico.java
@@ -0,0 +1,5 @@
+package com.example.gerconsulta;
+
+public class AdicionarMedico {
+
+}
---
+++ app/src/main/java/com/example/gerconsulta/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.gerconsulta;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.criarDB();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    private void criarDB(){
+        db = openOrCreateDatabase("consulta.db", Context.MODE_PRIVATE, null);
+        StringBuilder sql = new StringBuilder();
+
+        sql.append("CREATE TABLE IF NOT EXISTS medico (");
+        sql.append("_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+        sql.append("nome VARCHAR(50), ");
+        sql.append("crm VARCHAR(20), ");
+        sql.append("logradouro VARCHAR(100), ");
+        sql.append("numero MEDIUMINT(8), ");
+        sql.append("cidade VARCHAR(30), ");
+        sql.append("uf VARCHAR(2), ");
+        sql.append("celular VARCHAR(20), ");
+        sql.append("fixo VARCHAR(20)");
+        sql.append(");");
+
+        sql.append("CREATE TABLE IF NOT EXISTS paciente (");
+        sql.append("_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+        sql.append("nome VARCHAR(50), ");
+        sql.append("grp_sanguineo TINYINT(1), ");
+        sql.append("logradouro VARCHAR(100), ");
+        sql.append("numero MEDIUMINT(8), ");
+        sql.append("cidade VARCHAR(30), ");
+        sql.append("uf VARCHAR(2), ");
+        sql.append("celular VARCHAR(20), ");
+        sql.append("fixo VARCHAR(20)");
+        sql.append(");");
+
+        sql.append("CREATE TABLE IF NOT EXISTS paciente (");
+        sql.append("_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+        sql.append("paciente_id INTEGER NOT NULL, ");
+        sql.append("medico_id INTEGER NOT NULL, ");
+        sql.append("data_hora_inicio DATETIME, ");
+        sql.append("data_hora_fim DATETIME, ");
+        sql.append("observacao VARCHAR(200), ");
+        sql.append("FOREIGN KEY(paciente_id) REFERENCES paciente(id), ");
+        sql.append("FOREIGN KEY(medico_id) REFERENCES medico(id)");
+        sql.append(");");
+
+        try{
+            db.execSQL(sql.toString());
+        }catch(SQLException e){
+            Toast.makeText(getApplicationContext(),"Error" + e.getMessage(),Toast.LENGTH_SHORT);
+        }
+
+        db.close();
+    }
+}
